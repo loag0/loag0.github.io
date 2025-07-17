3e9bed233f600ef86b8bc810c550f931a0453b1a
Loago Moremi <loagomoremi@gmail.com>
2025-07-17 18:57:36 +0200

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ 
-    react(),
-    tailwindcss()
-  ],
-  base: "/loag0.github.io/"
-})
+  plugins: [react(), tailwindcss()],
+  base: "/loag0.github.io/",
+});
